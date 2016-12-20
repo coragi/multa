898e8cad83037cd83a4acf3ba4df29bafb565d5c
tiagoscotch <scotch.experi@gmail.com>
2016-12-19 22:32:07 -0200

    angular repeat get rolando

--- js/controller.js
+++ js/controller.js
@@ -2,18 +2,22 @@
 
   angular.module('myApp', [])
 
-  .controller('MainController', function($scope, $http) {
-	  $scope.teste = "TiagoScotch";
+  .controller('MainController', function($scope, $http, $interval) {
 
-	  $scope.buscalistaMulta = (function() {
-		  	var num=3;
-	        $http.get("http://www.portallbfv.com.br/original/portal/listaMulta.php?num=" + num)
-	          .then(function(resposta) {
-	            $scope.roubo = resposta.data;
-	          }, function(seErroUser) {
-	            $scope.error = "Digitou errado ai ANIMAL";
-	    });
-    })();
+		var num=0;
+	  var buscaRb = function() {
+				num++;
+				$http.get("http://www.portallbfv.com.br/original/portal/listaMulta.php?num=" + num)
+				  .then(function(resposta) {
+					$scope.roubo = resposta.data;
+				  }, function(seErroUser) {
+					$scope.error = "Digitou errado ai ANIMAL";
+			});
+			};
+
+		$scope.recebeRb = $interval(buscaRb,5000,10);
+
+
 
 
   })
